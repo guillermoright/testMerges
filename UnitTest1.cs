e166b68c656fbeb6a88300ec7e9e06131a9f78dd
Guillermo <guillermoright@gmail.com>
2023-11-03 07:41:43 +0100

    test firstcommit

---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using Testcontainers.Elasticsearch;
+using Xunit;
+
+public sealed class ElasticsearchContainerTest2 : IAsyncLifetime
+{
+    private readonly ElasticsearchContainer _elasticsearch
+        = new ElasticsearchBuilder().Build();
+
+    [Fact]
+    public async Task ReadFromElasticsearch()
+    {
+        var settings = new ElasticsearchClientSettings(new Uri(_elasticsearch.GetConnectionString()));
+        settings.ServerCertificateValidationCallback(CertificateValidations.AllowAll);
+
+        var client = new ElasticsearchClient(settings);
+
+        var stats = await client.PingAsync();
+
+        Assert.True(stats.IsValidResponse);
+    }
+
+    public Task InitializeAsync()
+        => _elasticsearch.StartAsync();
+
+    public Task DisposeAsync()
+        => _elasticsearch.DisposeAsync().AsTask();
+}
